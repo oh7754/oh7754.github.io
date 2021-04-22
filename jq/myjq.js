e9a776b4c6dab9326df701dc159cff254be9246b
oh7754 <44561211+oh7754@users.noreply.github.com>
2021-04-22 08:06:39 -0700

    Add files via upload

---
+++ jq/myjq.js
@@ -0,0 +1,45 @@
+
+// 21861077오진환
+
+$( document ).ready( function() {
+
+    $(".cbt1").mousedown(function() {
+
+            $(".con_1").slideToggle(700)
+            $(".c_nav").slideToggle(700)
+            $(".background_b").slideToggle(300)
+
+          });
+    $(".cbt2").mousedown(function() {
+          $(".con_2").slideToggle(700);
+          $(".c_nav").slideToggle(700)
+          $(".background_b").slideToggle(300)
+        });
+
+    $(".cbt3").mousedown(function() {
+          $(".con_3").slideToggle(700);
+          $(".c_nav").slideToggle(700)
+          $(".background").slideToggle(300)
+        });
+      $(".cbt4").mousedown(function() {
+            $(".con_4").slideToggle(700);
+            $(".c_nav").slideToggle(700)
+            // $(".background").slideToggle(300)
+        });
+      $(".cbt5").mousedown(function() {
+            $(".con_5").slideToggle(700);
+            $(".c_nav").slideToggle(700)
+            // $(".background").slideToggle(300)
+      });
+      $(".cbt6").mousedown(function() {
+            $(".con_6").slideToggle(700);
+            $(".c_nav").slideToggle(700)
+            // $(".background").slideToggle(300)
+      });
+
+
+});
+
+// $(".view_bt2").mousedown( function() {
+// $("body").css("background-color","yellow");
+// });
